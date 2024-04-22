6b5133598208bd0adc0434d4c88ba16548a42927
cagri coltekin <ccoltekin@sfs.uni-tuebingen.de>
2024-04-22 19:40:49 +0200

    Work on tira/docker evaluator.

---
+++ evaluation/evaluate.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import glob
+import re
+import csv
+import numpy as np
+from sklearn.metrics import precision_recall_fscore_support
+
+GOLD = os.environ.get('inputDataset', 'reference')
+PRED = os.environ.get('inputRun', 'predictions')
+OUTD = os.environ.get('outputDir', '.')
+
+pred_re = re.compile(
+    r"(?P<team>[^-]+)-(?P<task>[^-]+)-(?P<parl>[^-]{2}(-[^-]{2})?)-(?P<run>[^-]+)\.tsv")
+
+tasks = {'power', 'orientation'}
+parliaments = {'power': {'at', 'ba', 'be', 'bg', 'cz', 'dk', 'es',
+                         'es-ct', 'es-ga', 'es-pv', 'fi', 'fr', 'gb',
+                         'gr', 'hr', 'hu', 'it', 'lv', 'nl', 'pl',
+                         'pt', 'rs', 'si', 'tr', 'ua'},
+    'orientation': {'at', 'ba', 'be', 'bg', 'cz', 'dk', 'ee', 'es',
+                    'es-ct', 'es-ga', 'fi', 'fr', 'gb', 'gr', 'hr',
+                    'hu', 'is', 'it', 'lv', 'nl', 'no', 'pl', 'pt',
+                    'rs', 'se', 'si', 'tr', 'ua'}
+}
+
+def score(pred, task, parl, refdir=GOLD):
+    goldfile = os.path.join(refdir,
+            '-'.join((task, parl, 'labels')) + '.tsv')
+    goldids, goldlabels = [], []
+    with open(goldfile, 'rt') as f:
+        csvr = csv.reader(f, delimiter='\t')
+        for row in csvr:
+            id_, lab = row
+            goldids.append(id_)
+            goldlabels.append(int(lab))
+
+    predlabels = [round(pred[i]) for i in goldids]
+
+    p, r, f, supp = precision_recall_fscore_support(
+            goldlabels, predlabels, average='macro',
+            zero_division=0.0)
+    return p, r, f
+
+if __name__ == "__main__":
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--reference', '-r', default=GOLD)
+    ap.add_argument('--predictions', '-p', default=PRED)
+    ap.add_argument('--outdir', '-o', default=OUTD)
+    args = ap.parse_args()
+
+    print(args.reference, args.predictions, args.outdir)
+    print(glob.glob(args.predictions + '/*'))
+
+#    with ZipFile(args.predictions) as zf:
+#        for f in zf.namelist():
+#            print(os.path.basename(f))
+     
+#     teamname}-{args.task}-{pcode}-predictions.tsv"
+    
+    scores = dict()
+    predfiles = os.path.join(args.predictions, "*")
+    for predfile in glob.glob(predfiles):
+        fname = os.path.basename(predfile)
+        if not os.path.isfile(predfile) and os.path.getsize(predfile) == 0:
+            print(f"Skipping {fname}, no content.", file=sys.stderr)
+            continue
+        elif fname.lower().startswith('readme'): # skip quietly
+            continue
+        m = pred_re.match(fname)
+        if not m:
+            print(f"Skipping {fname}: file name pattern does not match.",
+                    file=sys.stderr)
+            continue
+        team, task = m.group('team'), m.group('task')
+        parl, run =  m.group('parl'), m.group('run')
+        if task not in tasks\
+                or parl not in parliaments[m.group('task')]:
+            print(f"Skipping {fname}:", end=" ", file=sys.stderr)
+            print(f"Unknown task or parliament or task '{task}' is not"
+                  f" available for parliament '{parl}'.", file=sys.stderr)
+            continue
+        print(f"Procesing {fname}.")
+        try:
+            with open(predfile, 'rt') as f:
+                pred = dict()
+                for line in f:
+                    id_, prob = line.strip().split('\t')
+                    pred[id_] = float(prob)
+            scores[(task, parl)] = score(pred, task, parl,
+                                         refdir=args.reference)
+        except:
+            print(f'Wrong file format {predfile}.', file=sys.stderr)
+            print('All rows of the prediction files should contain'
+                  'two columns(id, class/score) separated with a single tab.',
+                  file=sys.stderr)
+
+    if len(scores) == 0:
+        print(f'None of the input files are valid. No scores calcualted',
+                file=sys.stderr)
+        sys.exit(-1)
+
+    with open(os.path.join(args.outdir, 'evaluation.prototext'), 'wt') as outf:
+        pow_p, pow_r, pow_f = np.mean(
+                [scores[x] for x in scores if x[0] == 'power'],
+                axis=0)
+        ori_p, ori_r, ori_f = np.mean(
+                [scores[x] for x in scores if x[0] == 'orientation'],
+                axis=0)
+        print(f'measure{{\n  key: "F1_orientation"\n  value: "{ori_f}\n}}',
+                file=outf)
+        print(f'measure{{\n  key: "F1_power"\n  value: "{pow_f}\n}}',
+                file=outf)
+        for sc in sorted(scores):
+            task, parl = sc
+            for k, m in zip(("Precision", "Recall", "F1"), scores[sc]):
+                print(f'measure{{\n  key: "{k}_{task}_{parl}:"\n  value: "{m}\n}}',
+                        file=outf)
+
+#        print(f"team\ttask\tparliament\trun"
+#              f"\tprecision\trecall\tf1", file=outf)
+#        for sc in scores:
+#            team, task, parl, run = sc
+#            p, r, f = scores[sc]
+#            print(f"{team}\t{task}\t{parl}\t{run}"
+#                  f"\t{p}\t{r}\t{f}", file=outf)
--- linear-baseline.py
+++ linear-baseline.py
@@ -7,51 +7,108 @@ for details.
 import argparse
 import os
 import time
+import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_fscore_support
 from data import get_data
+import numpy as np
+from multiprocessing import Pool
 
 ap = argparse.ArgumentParser()
 ap.add_argument('parliaments', nargs="+")
-ap.add_argument('--task', '-t', default='power')
+ap.add_argument('--task', '-t', default='power',
+        choices=['power', 'orientation'])
 ap.add_argument('--data-dir', '-d', default='data')
 ap.add_argument('--C', '-C', type=int, default=1)
+ap.add_argument('--repeat', '-r', type=int, default=1)
+ap.add_argument('--nproc', '-j', type=int, default=4)
+ap.add_argument('--en-translation', '-e', action='store_true')
+ap.add_argument('--pred-dir', '-p', default='predictions')
+ap.add_argument('--save-models', '-s', default='models')
+ap.add_argument('--load-models', '-l')
+ap.add_argument('--teamname', '-T', default="baseline")
 args = ap.parse_args()
 args.parliaments = [x.lower() for x in args.parliaments]
 
-for pcode in args.parliaments:
-    # Train a logistic regression classifier, and pring evaluation
-    # metrics on a held-out data split.
-    start = time.time()
-    train_file = os.path.join(args.data_dir, args.task,
-                              f"{args.task}-{pcode}-train.tsv")
-    if not os.path.exists(train_file):
-        print(f"{pcode}: skipping, no training data.")
-        continue
-    t_trn, y_trn, t_val, y_val = get_data(train_file)
-    vec = TfidfVectorizer(sublinear_tf=True, analyzer="char",
-                          ngram_range=(1,3))
-    x_trn = vec.fit_transform(t_trn)
-    x_val = vec.transform(t_val)
-    m = LogisticRegression(max_iter=500, C=args.C)
-    m.fit(x_trn, y_trn)
-    pred = m.predict(x_val)
-    p, r, f, _ = precision_recall_fscore_support(
-            y_val, pred, average='macro', zero_division=0.0)
-    print(f"{pcode}: {100*p:.4f} / {100*r:.4f} / {100*f:.4f}"
-          f" [{time.time() - start:.2f}s]")
+if args.parliaments[0] == "all":
+    args.parliaments = ["at", "ba", "be", "bg", "cz", "dk", "ee",
+                        "es-ct", "es-ga", "es-pv", "es", "fi", "fr",
+                        "gb", "gr", "hr", "hu", "is", "it", "lv",
+                        "nl", "no", "pl", "pt", "rs", "se", "si",
+                        "tr", "ua"]
 
-    # If the corresponding test file exists, create the submission
-    # file with predictions.
-    test_file = os.path.join(args.data_dir, args.task,
-                              f"{args.task}-{pcode}-test.tsv")
-    if os.path.exists(test_file):
-        id_test, t_test, _ = get_data(test_file, testset=True)
-        x_test = vec.transform(t_test)
-        test_pred = m.predict_proba(x_test)
-        teamname = "baseline"
-        pred_file = f"{teamname}-{args.task}-{pcode}-predictions.tsv"
-        with open(pred_file, "wt") as fpred:
-            for i, p in enumerate(test_pred):
-                print(f"{id_test[i]}\t{p[1]}", file=fpred)
+def predict(pcode):
+    def write_predictions(m):
+        # If the corresponding test file exists, create the submission
+        # file with predictions.
+        test_file = os.path.join(args.data_dir, args.task,
+                                  f"{args.task}-{pcode}-test.tsv")
+        if os.path.exists(test_file):
+            id_test, t_test, _ = get_data(test_file, testset=True)
+            x_test = vec.transform(t_test)
+            test_pred = m.predict_proba(x_test)
+            pred_file = f"{args.teamname}-{args.task}-{pcode}-predictions.tsv"
+            os.makedirs(args.pred_dir, exist_ok=True)
+            with open(os.path.join(args.pred_dir, pred_file), "wt") as fpred:
+                for i, p in enumerate(test_pred):
+                    print(f"{id_test[i]}\t{p[1]}", file=fpred)
+    if args.load_models:
+        try:
+            model_file = f"{args.task}-{pcode}.joblib"
+            vec, m = joblib.load(
+                    os.path.join(args.load_models, model_file))
+        except:
+            m = None
+            print("Failed to read model from "
+                 f"{args.load_models}/{model_file}.")
+        if m: write_predictions(m)
+    else:
+        # Train a logistic regression classifier, and print evaluation
+        # metrics on a held-out data split.
+        start = time.time()
+        train_file = os.path.join(args.data_dir, args.task,
+                                  f"{args.task}-{pcode}-train.tsv")
+        if not os.path.exists(train_file):
+            print(f"{pcode}: skipping, no training data.")
+            return
+        P, R, F = [], [], []
+        best_F = 0.0
+        for i in range(args.repeat):
+            if pcode != "gb" and args.en_translation:
+                t_trn, y_trn, t_val, y_val = get_data(train_file,
+                        text_head='text_en')
+            else:
+                t_trn, y_trn, t_val, y_val = get_data(train_file)
+            vec = TfidfVectorizer(sublinear_tf=True, analyzer="char",
+                                  ngram_range=(1,3))
+            x_trn = vec.fit_transform(t_trn)
+            x_val = vec.transform(t_val)
+            m = LogisticRegression(max_iter=500, C=args.C)
+            m.fit(x_trn, y_trn)
+            pred = m.predict(x_val)
+            p, r, f, _ = precision_recall_fscore_support(
+                    y_val, pred, average='macro', zero_division=0.0)
+            P.append(p), R.append(r), F.append(f)
+
+            if f > best_F:
+                if args.save_models:
+                    os.makedirs(args.save_models, exist_ok=True)
+                    joblib.dump((vec, m), os.path.join(args.save_models,
+                        f"{args.task}-{pcode}.joblib"))
+                write_predictions(m)
+            if args.repeat == 1:
+                p, r, f = P[0], R[0], F[0]
+                print(f"{pcode}: {100*p:.4f} / {100*r:.4f} / {100*f:.4f}"
+                      f" [{time.time() - start:.2f}s]")
+            else:
+                p, psd = np.mean(P), np.std(P)
+                r, rsd = np.mean(R), np.std(R)
+                f, fsd = np.mean(F), np.std(F)
+                print(f"{pcode}: {100*p:.4f}±{100*psd:.4f} "
+                      f"{100*r:.4f}±{100*rsd:.4f} "
+                      f"{100*f:.4f}±{100*fsd:.4f} "
+                      f" [{(time.time() - start) / len(P):.2f}s]")
+
+pool = Pool(processes=args.nproc)
+pool.map(predict, args.parliaments)
